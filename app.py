f94569e626dabdbf04a124025198a9577502e4d6
Nafeesah_Aliyu_Isah <nafisaisah54@gmail.com>
2025-02-27 13:15:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ st.write('This app predicts the **Iris Flower** Type!')
 st.write('Please input the values below to get the prediction')
 
 # Input form
-sepal_ID =st.number_input('Sepal Length', min_value=0.0, max_value=10.0, value=5.0)
+sepal_ID =st.number_input('Sepal_ID', min_value=0.0, max_value=10.0, value=5.0)
+sepal_length =st.number_input('Sepal Length', min_value=0.0, max_value=10.0, value=5.0)
 sepal_width = st.number_input('Sepal Width', min_value=0.0, max_value=10.0, value=3.5)
 petal_length = st.number_input('Petal Length', min_value=0.0, max_value=10.0, value=1.5)
 petal_width = st.number_input('Petal Width', min_value=0.0, max_value=10.0, value=0.2)
